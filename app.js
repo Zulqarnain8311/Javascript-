af8bbe6f43cb76d3341c5fba6e49ef3010787fc9
Zulqarnain Haider <Zulqarnainhaider8311@gmail.com>
2024-11-30 08:44:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,351 @@
+// console.log("hello world");
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+// chapter no 1
+
+// alert("hello Sir ");
+
+// alert("Error ! please enter a valid password");
+
+// alert("welecome to js coding.... \n Happy Coding ");
+
+// alert("welecome to js coding.... ");
+// alert("Happy Coding \n prevent this page for addtional dialogs");
+
+// alert("hello... I can run js throuhg y browers consol ");
+// console.log(" hello... I can run js throuhg y browers consol");
+
+// done chapter no 1
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+// Chapyer no 2
+
+// var username = " Zulqarnain Haider";
+
+// var myname = " Zulqarnain Haider";
+
+// var message = "hello world";
+// alert(message);
+
+// var stdname = "zulqarnain haider";
+// alert(stdname);
+// var age = 18;
+// alert(age);
+// var work = "certified mobile application developing";
+// alert(work);
+
+// var pizza = "pizza \n pizz \n piz \n pi \n p ";
+// alert(pizza);
+
+// var email = "zh3203060380@gmail.com";
+// var message = "Its is your email  " + email;
+// alert(message);
+
+// var book = "A smater way to learn Javascript";
+// alert("I am trying to lrarn from thr book  " + book);
+
+// document.write("yah ! I can write a script html through js");
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+// done chapter no 02
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+// chapter  no   3
+
+// var age = "I am 15 old";
+// alert(age);
+
+// var visted = " you can visted at this web 154 times";
+// alert(visted);
+
+// var birthyear = "<h1> my birth year is 2002 </h1>";
+// document.write(birthyear);
+
+// var onlinestore =
+//   " Zulqranain ordered 15 T-shirt(medimum) on al-Aayat Clothing store.";
+// document.write(onlinestore);
+
+// done chapter no 03
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+// chapter no 04
+
+// var var1 , var2, var3 = "zulqarnain";
+
+// Legal Variable Names:
+
+// myVariable
+// _privateVar
+// $price
+// userName123
+// camelCaseExample
+
+// Illegal Variable Names:
+
+// 2ndVariable
+// my-variable
+// @invalidName
+// first name
+// var
+
+// var heading = "A heading stating “Rules for naming JS variables”";
+// document.write(heading);
+
+// document.write("Variable names in JavaScript can only contain letters (a-z, A-Z), digits (0-9), underscores (_), and dollar signs ($).")
+
+// document.write("Variables in JavaScript must begin with a letter, underscore (_), or dollar sign ($) character")
+
+// document.write("Variables name in Camelcase")
+
+// document.write("Variables name should not in js keywords")
+
+// done chapter no 04
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+// chapter no 05
+
+// document.write("<h1>  5 + 3 = 8  </h1>");
+
+// incomplete chapter no 05
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+// chapter no 06 to 09
+// document.write("value = 10");
+// document.write(" if a++ value is = 11  <br/>");
+// document.write(" current value is = 11  <br/>");
+
+// document.write(" if ++a value is = 12  <br/>");
+// document.write(" current value is = 12  <br/>");
+
+// document.write(" if a-- value is = 12 <br/>");
+// document.write(" current value is = 11<br/>");
+
+// document.write(" if --a value is = 11 <br/>");
+// document.write(" current value is = 10 <br/>");
+
+// var a = 2,
+//   b = 1;
+// var result = --a - --b + ++b + b--;
+
+// console.log(result);
+
+// var m = --a;
+// var n = --a - --b;
+// var o = --a - --b;
+// var p = --a - --b + ++b;
+// var q = --a - --b + ++b + b--;
+// console.log(m);
+// console.log(n);
+// console.log(o);
+// console.log(p);
+// console.log(q);
+
+// var username = prompt("Enter Your name");
+// var greeet = prompt("wellcome to our website");
+
+// var tablenubmer = prompt("Enter what you need");
+// for (var i = 1; i <= 10; ++i) {
+//   document.write(
+//     tablenubmer + " * " + i + " " + "=" + tablenubmer * i + "<br/>"
+//   );
+// }
+
+// Take three subjects' names from the user
+// const subject1 = prompt("Enter the name of the first subject:");
+// const subject2 = prompt("Enter the name of the second subject:");
+// const subject3 = prompt("Enter the name of the third subject:");
+
+// // Total marks for each subject
+// const totalMarks = 100;
+
+// // Take obtained marks for each subject from the user
+// const obtainedMarks1 = parseInt(
+//   prompt(`Enter obtained marks for ${subject1}:`)
+// );
+// const obtainedMarks2 = parseInt(
+//   prompt(`Enter obtained marks for ${subject2}:`)
+// );
+// const obtainedMarks3 = parseInt(
+//   prompt(`Enter obtained marks for ${subject3}:`)
+// );
+
+// // Calculate total marks and percentage
+// const totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+// const percentage = (totalObtainedMarks / (3 * totalMarks)) * 100;
+
+// // Display the result in a table
+// document.write("<table border='1'>");
+// document.write(
+//   "<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>"
+// );
+// document.write(
+//   `<tr><td>${subject1}</td><td>${totalMarks}</td><td>${obtainedMarks1}</td></tr>`
+// );
+// document.write(
+//   `<tr><td>${subject2}</td><td>${totalMarks}</td><td>${obtainedMarks2}</td></tr>`
+// );
+// document.write(
+//   `<tr><td>${subject3}</td><td>${totalMarks}</td><td>${obtainedMarks3}</td></tr>`
+// );
+// document.write("</table>");
+// document.write(`Total Obtained Marks: ${totalObtainedMarks}<br>`);
+// document.write(`Percentage: ${percentage.toFixed(2)}%`);
+
+// complete chapter no 06 to 09
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+// chapter no 09 to 11
+
+// var city = prompt("Enter Your City");
+// if ((city = " Karachi")) {
+//   console.log("wellome to the city of lights");
+// } else {
+//   document.write(" Get OUT >");
+// }
+
+// var gender = prompt("Enter Your gender");
+// if (gender == "male") {
+//   console.log("wellome  Sir");
+// } else if (gender == "female") {
+//   console.log(" Wellcome Maam");
+// } else {
+//   console.log("others are not allow");
+// }
+
+// var signalColour = prompt("Enter Your signal color");
+// if (signalColour == "red") {
+//   console.log("wellome  Please W8 for your turn");
+// } else if (signalColour == "yellow") {
+//   console.log(" Wellcome ready for going");
+// } else if (signalColour == "green") {
+//   console.log("go ");
+// } else {
+//   console.log("road pr to ao ");
+// }
+
+// var fuel = prompt("enter your fuel");
+
+// if (fuel > 0.25) {
+//   console.log("Allow go ");
+// } else {
+//   console.log(" please refill your catr fuel");
+// }
+
+// var a = 4;
+// if (++a === 5) {
+//   alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83) {
+//   alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13) {
+//   alert("condition 1 is true");
+// }
+// if (c === 13) {
+//   alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//   alert("condition 3 is true");
+// }
+// if (c === 14) {
+//   alert("condition 4 is true");
+// }
+
+// e
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//   alert("The cost equals");
+// }
+// if (true) {
+//   alert("True");
+// }
+// if (false) {
+//   alert("False");
+// }
+
+// var sceret = +prompt("Enter your number");
+// if (sceret == 5) {
+//   document.write("Bingo! Correct answer”");
+// } else {
+//   document.write(" Close enough to the correct answer”");
+// }
+
+// var tamperature = prompt("Enter temparature ");
+// if (tamperature > "40") {
+//   document.write("It is too hot outside.”");
+// } else if (tamperature > "30") {
+//   document.write("The Weather today is Normal.”.”");
+// } else if (tamperature > "20") {
+//   document.write("The Weather cool");
+// } else if (tamperature > "10") {
+//   document.write("OMG! Weather today is cool");
+// } else {
+//   document.write("please write some thing");
+// }
+
+// var num1 = +prompt("Enter your First Number");
+// var num2 = +prompt("Enter your Second Number");
+// var operator = prompt("+    -   *   /");
+// if (operator == "+") {
+//   document.write(num1 + num2);
+// } else if (operator == "-") {
+//   document.write(num1 - num2);
+// } else if (operator == "*") {
+//   document.write(num1 * num2);
+// } else if (operator == "/") {
+//   document.write(num1 / num2);
+// } else {
+//   document.write("please write some thing");
+// }
+
+// complete chapter no 09 to 11
+// ..
+// .
+// .
+// .
+// .
+// .
+// .
+//  Start chapter no 12 to 13
